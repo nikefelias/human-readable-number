5208bf85a35aebcdc325c5ad56767ab69f3fe4f3
«nikefelias» <felias@bk.ru>
2025-08-31 17:58:30 +0200

    feat: add a script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  const words = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+
+  const tens = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
+  const hundreds = [
+    '',
+    'one hundred',
+    'two hundred',
+    'three hundred',
+    'four hundred',
+    'five hundred',
+    'six hundred',
+    'seven hundred',
+    'eight hundred',
+    'nine hundred',
+  ];
+
+  const t = Math.floor(number / 10);
+  const u = number % 10;
+  const h = Math.floor(number / 100);
+  const rest = number % 100;
+
+  if (number < 20) {
+    return words[number];
+  }
+
+  if (number < 100) {
+    return `${tens[t]}${u > 0 ? ` ${words[u]}` : ''}`;
+  }
+
+  return `${hundreds[h]}${rest === 0 ? '' : ` ${toReadable(rest)}`}`;
 };
